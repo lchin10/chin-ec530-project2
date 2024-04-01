3554c02b743c5114851a8146d30d15fc0ecc29ac
unknown <lchin10@bu.edu>
2024-04-01 11:56:46 -0400

    changed address to render add

--- APIs/flask_run.py
+++ APIs/flask_run.py
@@ -4,7 +4,7 @@ from auth_api import auth_api_app
 from file_upload import file_upload_app
 
 app = Flask(__name__)
-CORS(app, origins='https://3.134.238.10')
+CORS(app, origins='https://chin-ec530-project2-2.onrender.com')
 app.config['UPLOAD_FOLDER'] = '../Uploads'
 app.config['SECRET_KEY'] = 'Tf&2L$nDp9@qBhV*WY3#Zc7u8Xe4@FmA'
 
